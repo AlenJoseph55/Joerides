ae1b72240f76b64d853f35b18e9b3e4d2c0bf965
AlenJoseph55 <alenjosephchamavila03@gmail.com>
2024-04-15 08:09:42 +0530

    Add Cycles page

--- src/app/cycles/page.tsx
+++ src/app/cycles/page.tsx
@@ -9,49 +9,40 @@ import {
 } from "@/components/ui/card"
 
 export default function cycles() {
+
+    const cycles = [
+        {name: "Avon", img:"", rate:"10"},
+        {name: "Hero", img:"", rate:"15"},
+        {name: "Hercules", img:"", rate:"20"},
+        {name: "Avon", img:"", rate:"10"},
+        {name: "Hero", img:"", rate:"15"},
+        {name: "Hercules", img:"", rate:"20"},
+        {name: "Avon", img:"", rate:"10"},
+        {name: "Hero", img:"", rate:"15"},
+        {name: "Hercules", img:"", rate:"20"},
+    ]
+
     return (
-        <div>
+        <div className="overflow-hidden">
             <Nav />
             <div className="p-7" id="heading">
                 <h1 className="text-3xl pt-3 pb-7">Cycles</h1>
-                <div className="flex items-center justify-evenly" id="cycles">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16" id="cycles">
+        {cycles.map(({name,rate})=>(
 
-                    <Card className="sm:w-[25%]">
-                        <CardHeader className="border-2 border-black mt-2 mb-2 text-center p-2">
-                            <p>Avon</p>
+            <Card className="border-0 w-[264px] sm:w-auto" key={name}>
+                        <CardHeader className="border-2 border-black mb-2 text-center p-2">
+                            <p>{name}</p>
                         </CardHeader>
-                        <CardContent>
-                            <p>Card Content</p>
+                        <img src="../../assets/cycles.png" alt=""/>
+                        <CardContent className="h-[17.7rem] sm:h-[18rem] md:h-[22rem]" id="cycle" >
                         </CardContent>
                         <CardFooter className="bg-yellow-500 p-2 items-center justify-center">
-                            <p>Card Footer</p>
+                            <p>`${rate}/hour`</p>
                         </CardFooter>
                     </Card>
 
-                    <Card className="sm:w-[25%]">
-                        <CardHeader className="border-2 border-black mt-2 mb-2 text-center p-2">
-                            <p>Avon</p>
-                        </CardHeader>
-                        <CardContent>
-                            <p>Card Content</p>
-                        </CardContent>
-                        <CardFooter className="bg-yellow-500 p-2 items-center justify-center">
-                            <p>Card Footer</p>
-                        </CardFooter>
-                    </Card>
-
-                    <Card className="sm:w-[25%]">
-                        <CardHeader className="border-2 border-black mt-2 mb-2 text-center p-2">
-                            <p>Avon</p>
-                        </CardHeader>
-                        <CardContent>
-                            <p>Card Content</p>
-                        </CardContent>
-                        <CardFooter className="bg-yellow-500 p-2 items-center justify-center">
-                            <p>Card Footer</p>
-                        </CardFooter>
-                    </Card>
-
+))}
                 </div>
             </div>
         </div>
